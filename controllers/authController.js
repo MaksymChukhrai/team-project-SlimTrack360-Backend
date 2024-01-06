d2ba128516a172764a88fb93a35945192566bc21
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-01-07 01:54:09 +0200

    Changed frontend URL

--- controllers/authController.js
+++ controllers/authController.js
@@ -166,7 +166,7 @@ const googleAuth = async (req, res) => {
   }
 
   res.redirect(
-    `https://maksymbora.github.io/team-project-SlimTrack360/verify?searchQuery=${verificationToken}`,
+    `https://maksymchukhrai.github.io/team-project-SlimTrack360/verify?searchQuery=${verificationToken}`,
   );
 };
 
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@
 
 - 🔗 API_URL https://healthyhub-emsa.onrender.com/api
 - 📃 API_DOCS https://healthyhub-emsa.onrender.com/api-docs
-- 🖼️ FRONTEND https://maksymbora.github.io/team-project-SlimTrack360
+- 🖼️ FRONTEND https://maksymchukhrai.github.io/team-project-SlimTrack360
